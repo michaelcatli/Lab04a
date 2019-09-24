ea0789b55c2d02254fe864ff89d2e69541bad5ca
Catli <10312@vtsd.com>
2019-09-24 08:45:05 -0400

    saved

--- Lab04avst.java
+++ Lab04avst.java
@@ -10,17 +10,15 @@ public class Lab04avst
 		System.out.println("Lab04a, Student Version\n");
 
 		double principal  = 250000;
-		double annualRate = 4.85;
+		double annualRate = .0485;
+		double mrate = annualRate/12;
 		double numYears   = 30;
-
-
-
-
-
-
-
-
-
-		System.out.println();
+		double numMonths = numYears*12;
+		System.out.println("Principal: $"+principal);
+		System.out.println("Annual rate: " + (annualRate*10*10) + "%");
+		System.out.println("Number of years: " + numYears);
+		double u = Math.pow((1+annualRate),numMonths);
+		double monthlyPayment = principal * ((mrate * u)/(u - 1));
+		System.out.println("Monthly Payment: $" + monthlyPayment);
 	}
 }
