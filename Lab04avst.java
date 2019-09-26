99411b00ea22972ab85fd90dea2d39799d2050dd
Catli <10312@vtsd.com>
2019-09-26 10:31:51 -0400

    finished 100pt

--- Lab04avst.java
+++ Lab04avst.java
@@ -8,7 +8,6 @@ public class Lab04avst
 	public static void main(String[] args)
 	{
 		System.out.println("Lab04a, Student Version\n");
-
 		double principal  = 250000;
 		double annualRate = .0485;
 		double mrate = annualRate/12;
@@ -17,8 +16,11 @@ public class Lab04avst
 		System.out.println("Principal: $"+principal);
 		System.out.println("Annual rate: " + (annualRate*10*10) + "%");
 		System.out.println("Number of years: " + numYears);
-		double u = Math.pow((1+annualRate),numMonths);
-		double monthlyPayment = principal * ((mrate * u)/(u - 1));
-		System.out.println("Monthly Payment: $" + monthlyPayment);
+		double u = Math.pow((1+mrate),numMonths);
+		double monthlyPayment = (Math.round(100*principal * ((mrate * u)/(u - 1))));
+		System.out.println("Monthly Payment: $" + monthlyPayment/100);
+		double totalPayments = numMonths*monthlyPayment/100;
+		System.out.println("Total Payments: $" + totalPayments);
+		System.out.println("Total Interest: $" + (totalPayments - principal));
 	}
 }
